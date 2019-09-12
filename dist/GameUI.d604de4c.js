b19bdc283e9d4c4edd0c4a9f8cdab95d06e8894c
strozs <andris.strozs@gmail.com>
2019-09-12 17:37:14 +0300

    dev and build

--- dist/GameUI.d604de4c.js
+++ dist/GameUI.d604de4c.js
@@ -659,7 +659,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53838" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54074" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
